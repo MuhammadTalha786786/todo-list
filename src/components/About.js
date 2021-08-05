0b4af2a1d981b8809f198833b713a47e5857b853
MuhammadTalha786786 <mtalha25800@gmail.com>
2021-08-05 20:52:57 +0500

    making img responsive

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,13 @@ function About() {
     <>
       <div className="text-center mt-3">
         <h5>Hello I am React JS developer</h5>
-        <img src={talha} alt="" />
+        <img
+          className=" rounded float-left img-responsive img-fluid mt-4"
+          height="300px"
+          width="300px"
+          src={talha}
+          alt=""
+        />
       </div>
     </>
   );
